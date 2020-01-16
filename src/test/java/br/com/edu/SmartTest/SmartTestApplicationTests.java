6555f1e2bfd9517889538528d90bb53e00f976b7
giovane <giovanehl2@gmail.com>
2020-01-15 23:07:20 -0300

    versão inicial

---
+++ src/test/java/br/com/edu/SmartTest/SmartTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edu.SmartTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
